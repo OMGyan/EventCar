69e93e93776cf7bbf21f1602b7742a0812839ed3
yanxun <1768695841@qq.com>
2019-08-06 17:29:38 +0800

    框架及使用demo完成

--- app/src/main/java/com/yx/eventcar/MainActivity.java
+++ app/src/main/java/com/yx/eventcar/MainActivity.java
@@ -1,7 +1,14 @@
 package com.yx.eventcar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.yx.eventcarlib.EventCar;
+import com.yx.eventcarlib.Subscribe;
+import com.yx.eventcarlib.ThreadMode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EventCar.getDefault().register(this);
+    }
+
+    public void jump(View view) {
+        startActivity(new Intent(this,SecondActivity.class));
+    }
+
+    @Subscribe(threadMode = ThreadMode.BACKGROUND)
+    public void getInfo(User user){
+        Log.i("yx", "getInfo: "+user.toString()+"-------"+Thread.currentThread().getName());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventCar.getDefault().unregister(this);
     }
 }
